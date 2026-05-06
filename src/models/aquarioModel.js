5f22a0fdfd7b5af4ecabc49c34ad30be9f2e5794
Giu Bortoletto <giulia.bortoletto@sptech.school>
2026-05-05 21:35:55 -0300

    Juntei a API web-data-viz ao meu projeto, inseri os dados na tabela do banco e direcionei o login a tela de dashboard

--- src/models/aquarioModel.js
+++ src/models/aquarioModel.js
@@ -1,23 +1,23 @@
 var database = require("../database/config");
 
+/*
 function buscarAquariosPorEmpresa(empresaId) {
 
   var instrucaoSql = `SELECT * FROM aquario a WHERE fk_empresa = ${empresaId}`;
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
-}
+} 
+*/
 
-function cadastrar(empresaId, descricao) {
+function cadastrar(email, senha) {
   
-  var instrucaoSql = `INSERT INTO (descricao, fk_empresa) aquario VALUES (${descricao}, ${empresaId})`;
+  var instrucaoSql = `INSERT INTO (email, senha) usuario VALUES (${email}, ${senha})`;
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
 }
 
-
 module.exports = {
-  buscarAquariosPorEmpresa,
-  cadastrar
+  
 }
--- src/models/empresaModel.js
+++ src/models/empresaModel.js
@@ -1,27 +1,28 @@
 var database = require("../database/config");
 
 function buscarPorId(id) {
-  var instrucaoSql = `SELECT * FROM empresa WHERE id = '${id}'`;
+  var instrucaoSql = `SELECT * FROM usuario WHERE id = '${id}'`;
 
   return database.executar(instrucaoSql);
 }
 
 function listar() {
-  var instrucaoSql = `SELECT id, razao_social, cnpj, codigo_ativacao FROM empresa`;
+  var instrucaoSql = `SELECT id, nome, email, senha FROM usuario`;
 
   return database.executar(instrucaoSql);
 }
-
+/*
 function buscarPorCnpj(cnpj) {
   var instrucaoSql = `SELECT * FROM empresa WHERE cnpj = '${cnpj}'`;
 
   return database.executar(instrucaoSql);
 }
+  */
 
-function cadastrar(razaoSocial, cnpj) {
-  var instrucaoSql = `INSERT INTO empresa (razao_social, cnpj) VALUES ('${razaoSocial}', '${cnpj}')`;
+function cadastrar(nome, email, senha) {
+  var instrucaoSql = `INSERT INTO usuario (nome,email, senha) VALUES ('${nome}', '${email}', '${senha}')`;
 
   return database.executar(instrucaoSql);
 }
 
-module.exports = { buscarPorCnpj, buscarPorId, cadastrar, listar };
+module.exports = { buscarPorId, cadastrar, listar };
