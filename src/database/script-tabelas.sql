52341d7e644807ba4d1318c8a07ff24b940be00b
Giu Bortoletto <giulia.bortoletto@sptech.school>
2026-05-05 21:40:47 -0300

    Subi meu script do banco de dados para cá

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -6,24 +6,19 @@
 comandos para mysql server
 */
 
-CREATE DATABASE aquatech;
 
-USE aquatech;
+CREATE DATABASE projetoPessoal;
 
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
-);
+USE projetoPessoal;
+
+GRANT ALL PRIVILEGES ON projetopessoal.* TO 'usuario'@'%';
+FLUSH PRIVILEGES;
 
 CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+	senha VARCHAR(50)
 );
 
 CREATE TABLE aviso (
@@ -34,29 +29,8 @@ CREATE TABLE aviso (
 	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+insert into usuario (nome, email,senha) values ('Giulia', 'giulia@gmail.com','123456');
+insert into usuario (nome, email,senha) values ('Caua', 'Caua@gmail.com','abcdef');
 
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+select * from  usuario;
